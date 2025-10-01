45717ba98857108855d9b1069ce611ef6c7e6d3a
Mohamad Hambali Bin Mhd Noor <megapersona95@yahoo.com>
2025-10-02 07:49:41 +0800

    Professor Oak’s Challenge:: tell us your name.

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    string player_name;
+
+    cout << "Enter your name: ";
+    cin >> player_name;
+
+    cout << "Greeting~ " << player_name << ", Welcome to the Pokemon World!" << endl;
 
     return 0;
 }
